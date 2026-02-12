4010a4242ba981d95c99eab398fa427b13a490a4
srikanta0427 <srikanta0427@gmail.com>
2026-02-13 03:13:16 +0530

    Create Sum of first last element in array.java

---
+++ Basics/Sum of first last element in array.java
@@ -0,0 +1,6 @@
+// Given an integer array nums, return the sum of the 1st and last element of the array.
+class Solution {
+    public int sumOfFirstAndLast(int[] nums) {
+        return (nums[0]+nums[nums.length-1));
+    }
+}
